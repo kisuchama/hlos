6a24df40d324403f6526d200f599c53716c65282
Jin <jin@kisu.moe>
2022-06-18 21:24:21 -0700

    resolved unique key prop error

--- components/StoryPart.js
+++ components/StoryPart.js
@@ -6,15 +6,15 @@ export default function StoryPart({ part }) {
         pt = '0' + pt
     }
     return (
-        <>
-            <div className="flex flex-row mx-2 h-20 items-center justify-start">
-                <h2 className="text-4xl font-bold leading-0 -mt-2 mr-4">{pt}</h2>
-                {part.cameos.map((c, i) => (
-                    c.hero.chibi ? (
-                     <Cameo key={i} cameo={c} />
-                    ) : (<></>)
-                ))}
-            </div>
-        </>
+        <div className="flex flex-row mx-2 h-20 items-center justify-start">
+            <h2 className="text-4xl font-bold leading-0 -mt-2 mr-4">{pt}</h2>
+            {part.cameos.map((c, i) => (
+                <div key={i}>
+                    {c.hero.chibi ? (
+                        <Cameo cameo={c} />
+                    ) : (<></>)}
+                </div>
+            ))}
+        </div>
     )
 }
