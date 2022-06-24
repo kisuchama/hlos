cc4f4556dcef5df91edb7d180ec08d9455423fc9
Jin <jin@kisu.moe>
2022-06-23 19:26:18 -0700

    append character tags for filtering

--- components/Filter.js
+++ components/Filter.js
@@ -1,43 +1,57 @@
 import { MdFilterList } from "react-icons/md"
 import { useState } from "react";
+import Image from "next/image";
 
-export async function getStaticProps() {
-    const heroes = await prisma.hero.findMany({
-        where: {
-            chibi: true,
-        },
-        select: {
-            name: true,
-        },
-    })
+// export async function getStaticProps() {
+//     const heroes = await prisma.hero.findMany({
+//         // where: {
+//         //     id: { lte: 16 },
+//         // },
+//         select: {
+//             name: true,
+//         },
+//     })
 
-    return {
-        props: { heroes }
-    }
-}
+//     return {
+//         props: { heroes }
+//     }
+// }
 
-export default function StoryFilter({ heroes }) {
-    const [showMe, setShowMe] = useState(false);
+export default function StoryFilter() {
+    const [showFilters, setShowFilters] = useState(false);
+    // const [showPart, setShowPart] = useState(false);
     function toggleFilters() {
-        setShowMe(!showMe)
+        setShowFilters(!showFilters)
     }
+    // function filterChara(chara) {
+    //     setShowPart(!showPart)
+    // }
+
+    const heroes = [
+        'Akira','Will','Brad','Oscar',
+        'Ren','Gast','Victor','Marion',
+        'Junior','Faith','Keith','Dino',
+        'Gray','Billy','Asch','Jay']
     return (
         <>
-            <button id="filterToggle" className={`fixed z-50 right-12 top-12 bg-black p-2 ${showMe ? "mr-64" : ""}`} onClick={toggleFilters}>
+            <button id="filterToggle" className={`fixed z-50 right-12 top-12 bg-black p-2 ${showFilters ? "mr-64" : ""}`} onClick={toggleFilters}>
                 <MdFilterList className="text-4xl text-white" />
             </button>
-            <div id="storyFilters" className={`h-screen w-full bg-white sm:w-80 fixed z-40 top-0 -right-80 overflow-x-hidden p-12 border-l-[3px] ${showMe ? "mr-80" : ""}`}>
-                <h3 className="text-3xl font-display uppercase">Filters</h3>
-                {/* {heroes.map((h, i) => (
-                    <a key={i} className="h-80px w-88px relative inline-block">
+            <div id="storyFilters" className={`h-screen w-full bg-white sm:w-80 fixed z-40 top-0 -right-80 overflow-x-hidden p-12 border-l-[3px] ${showFilters ? "mr-80" : ""}`}>
+                <h1 className="text-3xl font-display uppercase">Filters</h1>
+                <h2 className="text-2xl font-display mt-8">Characters</h2>
+                <div className="grid grid-cols-4 mt-4 gap-2">
+                {heroes.map((h, i) => (
+                    <a key={i}>
                         <Image
-                            src={`/images/chibi/${h.name}.png`}
+                            src={`/images/chibi/${h.toLowerCase()}.png`}
                             alt={h.name}
                             width={66}
                             height={60} 
                         />
                     </a>
-                ))} */}
+                ))}
+                </div>
             </div>
         </>
     )
--- components/StoryPart.js
+++ components/StoryPart.js
@@ -5,8 +5,15 @@ export default function StoryPart({ part }) {
     if (pt.length == 1) {
         pt = '0' + pt
     }
+
+    const partCameos = []
+    var h = ''
+    for ( var i=0; i < part.cameos.length; i++) {
+        partCameos.push('tag' + part.cameos[i].hero.name)
+    }
+
     return (
-        <div className="flex flex-row mx-2 h-20 items-center justify-start">
+        <div className={`flex flex-row mx-2 h-20 items-center justify-start partDiv ${partCameos}`}>
             <h2 className="text-4xl font-display font-bold leading-0 -mt-2 mr-4 w-[66px]">{pt}</h2>
             {part.cameos.map((c, i) => (
                 <div key={i}>
